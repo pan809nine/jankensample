ae382a261c9212af4f9665bbd549cb9e004482e4
pan809nine <52058511+pan809nine@users.noreply.github.com>
2020-07-12 12:12:25 +0900

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    score:''
   },
   mutations: {
+    onSelected(state,payload){
+      state.score += payload + " "
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters:{
+    score:state => {
+      return state.score
+    }
   }
 })
